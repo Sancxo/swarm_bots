8d6ff5b04fb98590842b481edc445bef5f320943
Simon <simon@7lieues.io>
2023-01-19 16:46:00 +0100

    refactor(demo_live): refactors pattern match for render_led function

--- lib/swarm_bots_web/live/demo_live.ex
+++ lib/swarm_bots_web/live/demo_live.ex
@@ -4,7 +4,7 @@ defmodule SwarmBotsWeb.DemoLive do
   alias SwarmBots.Demo.{Game, Bot}
 
   def mount(_params, _session, socket) do
-    if connected?(socket), do: :timer.send_interval(50, :tick)
+    if connected?(socket), do: :timer.send_interval(12, :tick)
     {:ok, socket |> assign(game: Game.new_game())}
   end
 
@@ -30,73 +30,31 @@ defmodule SwarmBotsWeb.DemoLive do
     """
   end
 
-  defp render_led(%Bot{position: {x, y}, rotation: 0} = assigns) do
-    ~H"""
-    <circle cx={x} cy={y + 10} r="2" fill="chartreuse">
-      <%= animate_led(%{}) %>
-    </circle>
-    """
-  end
+  defp render_led(%Bot{position: {x, y}, rotation: 0}), do: render_led(%{x: x, y: y + 10})
 
-  defp render_led(%Bot{position: {x, y}, rotation: 45} = assigns) do
-    ~H"""
-    <circle cx={x - 7.5} cy={y + 7.5} r="2" fill="chartreuse">
-      <%= animate_led(%{}) %>
-    </circle>
-    """
-  end
+  defp render_led(%Bot{position: {x, y}, rotation: 45}), do: render_led(%{x: x - 7.5, y: y + 7.5})
 
-  defp render_led(%Bot{position: {x, y}, rotation: 90} = assigns) do
-    ~H"""
-    <circle cx={x - 10} cy={y} r="2" fill="chartreuse">
-      <%= animate_led(%{}) %>
-    </circle>
-    """
-  end
+  defp render_led(%Bot{position: {x, y}, rotation: 90}), do: render_led(%{x: x - 10, y: y})
 
-  defp render_led(%Bot{position: {x, y}, rotation: 135} = assigns) do
-    ~H"""
-    <circle cx={x - 7.5} cy={y - 7.5} r="2" fill="chartreuse">
-      <%= animate_led(%{}) %>
-    </circle>
-    """
-  end
+  defp render_led(%Bot{position: {x, y}, rotation: 135}),
+    do: render_led(%{x: x - 7.5, y: y - 7.5})
 
-  defp render_led(%Bot{position: {x, y}, rotation: 180} = assigns) do
-    ~H"""
-    <circle cx={x} cy={y - 10} r="2" fill="chartreuse">
-      <%= animate_led(%{}) %>
-    </circle>
-    """
-  end
+  defp render_led(%Bot{position: {x, y}, rotation: 180}), do: render_led(%{x: x, y: y - 10})
 
-  defp render_led(%Bot{position: {x, y}, rotation: 225} = assigns) do
-    ~H"""
-    <circle cx={x + 7.5} cy={y - 7.5} r="2" fill="chartreuse">
-      <%= animate_led(%{}) %>
-    </circle>
-    """
-  end
+  defp render_led(%Bot{position: {x, y}, rotation: 225}),
+    do: render_led(%{x: x + 7.5, y: y - 7.5})
 
-  defp render_led(%Bot{position: {x, y}, rotation: 270} = assigns) do
-    ~H"""
-    <circle cx={x + 10} cy={y} r="2" fill="chartreuse">
-      <%= animate_led(%{}) %>
-    </circle>
-    """
-  end
+  defp render_led(%Bot{position: {x, y}, rotation: 270}),
+    do: render_led(%{x: x + 10, y: y})
 
-  defp render_led(%Bot{position: {x, y}, rotation: 315} = assigns) do
-    ~H"""
-    <circle cx={x + 7.5} cy={y + 7.5} r="2" fill="chartreuse">
-      <%= animate_led(%{}) %>
-    </circle>
-    """
-  end
+  defp render_led(%Bot{position: {x, y}, rotation: 315}),
+    do: render_led(%{x: x + 7.5, y: y + 7.5})
 
-  def animate_led(assigns) do
+  defp render_led(%{x: x, y: y} = assigns) do
     ~H"""
-    <animate attributeName="opacity" values="1;0;1" dur="2s" repeatCount="indefinite" />
+    <circle cx={x} cy={y} r="2" fill="chartreuse">
+      <animate attributeName="opacity" values="1;0;1" dur="2s" repeatCount="indefinite" />
+    </circle>
     """
   end
 end
